fcee20b55370c04ab3a65ec4bca0d699e9f8e6ba
Nikita Pershin <pershin@delta-vrn.ru>
2026-06-15 18:12:25 +0300

    Реализованы конструктор и деструктор класса ClientHandler

--- clienthandler.cpp
+++ clienthandler.cpp
@@ -1,5 +1,34 @@
 #include "clienthandler.h"
+#include <QThread>
 
-ClientHandler::ClientHandler(QObject *parent)
-    : QObject{parent}
-{}
+
+ClientHandler::ClientHandler(QTcpSocket *socket, QObject *parent)
+    : QObject(parent), m_socket(socket)
+{
+    QString connName = "db_conn_" + QString::number((quintptr)QThread::currentThreadId());
+    m_db = QSqlDatabase::addDatabase("QSQLITE", connName);
+    m_db.setDatabaseName("users.db");
+}
+
+ClientHandler::~ClientHandler()
+{
+    if(m_socket) m_socket->deleteLater();
+    QString connName = m_db.connectionName();
+    m_db = QSqlDatabase();
+    QSqlDatabase::removeDatabase(connName);
+}
+
+void ClientHandler::startProcessing()
+{
+
+}
+
+void ClientHandler::onReadyRead()
+{
+
+}
+
+void ClientHandler::onDisconnected()
+{
+
+}
